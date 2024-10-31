9d441b47d2e802c494f9250d60f010170df53e45
Glioh <mtl5475@psu.edu>
2024-10-31 12:47:49 -0400

    Created PlayerPlayView. Have not made it clickable or interactable

--- frontend/src/lib/play/QuizChoiceCard.svelte
+++ frontend/src/lib/play/QuizChoiceCard.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     import type { QuizChoice } from '../../model/quiz'; // Adjust the import path as necessary
     
-    export let choice: QuizChoice;
     export let color: string;
 </script>
 
-<div CLASS="w-1/2 text-white font-bold flex items-center text-3x1 pl-14 {color}">
-    {choice.name}
+<div CLASS="w-1/2 text-white font-bold flex items-center text-5xl pl-14 {color}">
+    <slot />
 </div>
--- frontend/src/views/host/HostPlayView.svelte
+++ frontend/src/views/host/HostPlayView.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
    import QuizChoiceCard from "../../lib/play/QuizChoiceCard.svelte";
+    import { COLORS } from "../../model/quiz";
    import { type HostGame, tick, currentQuestion, state } from "../../service/host/host";
-
-    export const COLORS = ["bg-pink-400", "bg-blue-400", "bg-yellow-400", "bg-purple-400"];
 
     export let game: HostGame;
 </script>
@@ -13,20 +12,24 @@
             {$currentQuestion.name}
         </div>
         <div class="flex-1 flex flex-col justify-center pl-4">
-            <div
-                class="bg-purple-500 text-white text-3xl p-4 rounded-lg mb-4"
-            >
-                {$tick}
+            <div class="flex justify-between items-center">
+                <div
+                    class="bg-purple-500 text-white text-3xl p-4 rounded-lg mb-4"
+                >
+                    {$tick}
+                </div>
+                <img
+                    alt="center"
+                    class="max-w-[500px]"
+                />
+                <div class="w-24"></div>
             </div>
-            <img
-                alt="center"
-                class="max-w-[500px]"
-            />
-            <div class="w-24"></div>
         </div>
         <div class="flex flex-wrap w-full h-96">
             {#each COLORS as color, i}
-                <QuizChoiceCard {color} choice={$currentQuestion.choices[i]} />
+                <QuizChoiceCard {color}>
+                    <p class="pl-14">{$currentQuestion.choices[i].name}</p>
+                </QuizChoiceCard>
             {/each}
         </div>
     </div>
